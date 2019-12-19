5d53bc75622a2f4dc874409d70bd8afb35d7a032
ahyunlee5 <50925948+ahyunlee5@users.noreply.github.com>
2019-12-19 13:59:02 -0800

    Add files via upload

--- list.js
+++ list.js
@@ -12,15 +12,23 @@ function List(props) {
                 {props.cards.map((card) =>
                 <Card 
                     key={card.id}
+                    id={card.id}
                     title={card.title}
-                    content={card.content}/>
+                    content={card.content}
+                    onClickDelete={props.onClickDelete}
+                    />
                 )}
-                <button type='button' className='List-add-button'>
+                <button type='button' className='List-add-button'
+                    onClick={() => props.onClickAdd(props.id)}>
                     + Add Random Card
                 </button>
             </div>
         </section>
     )
+}
+
+List.defaultProps = {
+    onClickAdd: () => {},
 }
 
 export default List;
